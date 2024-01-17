6ef7730845659b49d9cc13b1c548fbd5dfe8edf4
Satriyo Unggul Wicaksono <stryuw@gmail.com>
2024-01-17 10:21:04 +0700

    refactor code

--- src/app.js
+++ src/app.js
@@ -5,45 +5,56 @@ import fs from "fs";
 import checkFolder from "./checkFolder.js";
 
 const app = express();
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-  let date = new Date();
-  res.send("Status Ok! " + date.toLocaleString() + " WAHL");
+  const date = new Date();
+  res.send(`Status Ok! ${date.toLocaleString()} WAHL`);
 });
 
-let date = new Date();
-let year = date.getFullYear();
-let month = date.getMonth() + 1;
-let day = date.getDate();
-let hour = date.getHours();
-console.log("Status Ok! " + date.toLocaleString());
+const createBackupPath = () => {
+  const paths = ["./backup", `./backup/${year}`, `./backup/${year}/${month}`, `./backup/${year}/${month}/RAOB`];
+  paths.forEach((path) => checkFolder(path));
+};
 
-const autocheck = () => {
-  checkFolder("./backup");
-  checkFolder("./backup/" + year);
-  checkFolder("./backup/" + year + "/" + month);
-  checkFolder("./backup/" + year + "/" + month + "/RAOB");
-}
-
-cron.schedule("44 9 * * *", () => {
-  let img;
-  axios
-    .get('http://172.19.2.99/monitoring_rason/status_rason/WAHL')
+const saveRaobFile = (utc) => {
+  axios.get('http://172.19.2.99/monitoring_rason/status_rason/WAHL')
     .then(function (response) {
-      img = response.data.gambar.split(';base64,').pop();
+      const img = response.data.gambar.split(';base64,').pop();
       const buffer = Buffer.from(img, 'base64');
-      autocheck();
-      fs.writeFileSync(`./backup/${year}/${month}/RAOB/${day}_${month}_${year}_raob_00UTC.png`, buffer);
+      createBackupPath();
+      const fileSuffix = utc === 0 ? "00" : "12";
+      const fileName = `./backup/${year}/${month}/RAOB/${day}_${month}_${year}_raob_${fileSuffix}UTC.png`;
+      fs.writeFileSync(fileName, buffer);
+      console.log(`Status Ok! Save RAOB FILE ${fileSuffix} UTC => ${fileName}`);
     })
     .catch(function (error) {
       console.log(error);
     });
+};
 
-    console.log("Status Ok! Save RAOB FILE 00 UTC => " + date.toLocaleString());
-});
+const scheduleRaobJobs = () => {
+  cron.schedule("45 9 * * *", () => saveRaobFile(0)); // 00 UTC
+  cron.schedule("45 21 * * *", () => saveRaobFile(12)); // 12 UTC
+};
 
-autocheck();
+const initializeServer = () => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+};
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000");
-});
+const initializeApp = () => {
+  const date = new Date();
+  global.year = date.getFullYear();
+  global.month = date.getMonth() + 1;
+  global.day = date.getDate();
+  global.hour = date.getHours();
+  console.log(`Status Ok! ${date.toLocaleString()}`);
+
+  scheduleRaobJobs();
+  createBackupPath();
+  initializeServer();
+};
+
+initializeApp();
